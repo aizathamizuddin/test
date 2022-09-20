58126c05264a46066a4dce883c30935ca2ed0295
aizathamizuddin <aizathamizuddin@gmail.com>
2022-09-20 18:53:11 +0900

    Added comments in main.js

--- main.js
+++ main.js
@@ -14,8 +14,13 @@ console.log(div(10,5));
 
 /*
 Practice 4 (accidentally commit a BIG MISTAKE)
-    - git reset --soft HEAD~
+    - git reset --soft HEAD~    (resets to STAGED)
     - git restore --staged main.js
     - git restore main.js
 */
 
+/*
+Practice 5 (accidentally commit a BIG MISTAKE)
+    - git reset HEAD~           (resets to UNSTAGED)
+    - git restore main.js
+*/
